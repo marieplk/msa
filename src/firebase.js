6803944a42bea9dace98f6da69a56116ddd91eb1
marieplk <mashurka.p@gmail.com>
2024-06-03 20:48:11 +0300

    database

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore' //Подключение хранилища
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCZbbh8t3YCWcCpKL7rU2C7uMxa9GIOxqc",
-  authDomain: "msa-msa-9b85a.firebaseapp.com",
-  projectId: "msa-msa-9b85a",
-  storageBucket: "msa-msa-9b85a.appspot.com",
-  messagingSenderId: "827617978666",
-  appId: "1:827617978666:web:6bf09e527dffbc4bba1653"
+  apiKey: "AIzaSyDPqMVl1YTkJZGI0IGrv9peMe93gRYcDlQ",
+  authDomain: "plaksina-doc.firebaseapp.com",
+  projectId: "plaksina-doc",
+  storageBucket: "plaksina-doc.appspot.com",
+  messagingSenderId: "23491871370",
+  appId: "1:23491871370:web:c0dbd9f3d202d40a742373"
 };
 
 // Initialize Firebase
