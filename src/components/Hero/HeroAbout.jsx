33aaaf3a2c55cf756f3fe955f05f2e167be964d0
marieplk <mashurka.p@gmail.com>
2024-06-20 18:26:39 +0300

    new

--- src/components/Hero/HeroAbout.jsx
+++ src/components/Hero/HeroAbout.jsx
@@ -20,7 +20,7 @@ const HeroAbout = () => {
           </Link>
       </div>
       <div className={styles.heroImage}>
-        <img src={Hero1} alt="" />
+        <img src={Hero3} alt="" />
       </div>
     </div>
   );
